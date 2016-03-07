0c3b7aac483fbe7fc350e90820abac4554eef921
yu-si <yut-sim@amail.plala.or.jp>
2016-03-08 00:04:23 +0900

    no message

---
+++ 04_app/python/intelligence/const/Configure.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# Config
+#
+#
+#
+
+class Common:
+    TIME = "TIME"
+
+
+class TissueType:
+    EAR = "EAR"
+    EYE = "EYE"
+
+class OS:
+    FILE_PATH = "output/log.txt"
+    FILE_MODE = "a"
